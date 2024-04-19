c2733fae59f323609e2e90e768cae5753dc93056
lijq17794802 <54316063+lijq17794802@users.noreply.github.com>
2024-04-19 18:17:11 +0800

    webui.py

--- webui.py
+++ webui.py
@@ -75,7 +75,7 @@ def webui():
                 auto_launch_browser = True
             elif shared.opts.auto_launch_browser == "Local":
                 auto_launch_browser = not cmd_opts.webui_is_non_local
-        print('部署成功，记得打赏作者 Nyan9哦')
+        print('部署成功，记得打赏作者 Leeo')
         app, local_url, share_url = shared.demo.launch(
             share=cmd_opts.share,
             server_name=initialize_util.gradio_server_name(),
